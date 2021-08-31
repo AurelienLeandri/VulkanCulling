830131c6694fabc72e5040c4446a3f9d03ea049c
AurelienLeandri <aurelien.leandri@gmail.com>
2021-08-31 11:32:10 +0200

    [Scene] Add base interface for shapes (sampling, area and transform handle)

--- LeoEngine/Include/LeoEngine/Scene/Shapes/Shape.h
+++ LeoEngine/Include/LeoEngine/Scene/Shapes/Shape.h
@@ -1,6 +1,26 @@
 #pragma once
 
+#include <GeometryIncludes.h>
+
+#include <memory>
+
 namespace leo {
+	struct HitRecord;
+	class Transform;
+
 	class Shape {
+	public:
+		virtual float area() const = 0;
+
+		virtual glm::vec3 sample(const HitRecord& record, float& pdf) const = 0;
+
+		virtual float pdf(const glm::vec3& point, const HitRecord& record) const = 0;
+
+	public:
+		const std::shared_ptr<const Transform> getTransform() const;
+		void setTransform(std::shared_ptr<const Transform> transform);
+
+	private:
+		std::shared_ptr<const Transform> _transform;
 	};
 }
--- LeoEngine/Sources/Scene/Shapes/Shape.cpp
+++ LeoEngine/Sources/Scene/Shapes/Shape.cpp
@@ -1,1 +1,15 @@
 #include <Scene/Shapes/Shape.h>
+
+namespace leo {
+
+	const std::shared_ptr<const Transform> Shape::getTransform() const
+	{
+		return _transform;
+	}
+
+	void Shape::setTransform(std::shared_ptr<const Transform> transform)
+	{
+		_transform = transform;
+	}
+
+}
