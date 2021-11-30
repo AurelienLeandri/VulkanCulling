bb7e8a8b3e988389f0547310c956f21fc9ad005d
AurelienLeandri <aurelien.leandri@gmail.com>
2021-11-30 15:06:00 +0100

    [Renderer] Add depth pyramid compute shader

--- Resources/Shaders/compile.bat
+++ Resources/Shaders/compile.bat
@@ -1,4 +1,5 @@
 C:/VulkanSDK/1.2.154.1/Bin32/glslc.exe shader.vert -o vert.spv
 C:/VulkanSDK/1.2.154.1/Bin32/glslc.exe shader.frag -o frag.spv
 C:/VulkanSDK/1.2.154.1/Bin32/glslc.exe indirect_cull.comp  -o indirect_cull.spv
+C:/VulkanSDK/1.2.154.1/Bin32/glslc.exe depth_pyramid.comp  -o depth_pyramid.spv
 pause
