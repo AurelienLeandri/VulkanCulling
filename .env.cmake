5611e02f6f9d97373c26a396b438a4ed93ab8ea0
AurelienLeandri <aurelien.leandri@gmail.com>
2022-01-08 14:59:43 +0100

    [General] Add .env.cmake and fix copy past problems in it

---
+++ .env.cmake
@@ -0,0 +1,9 @@
+set (GLFW_LIB_PATH ".\external\lib")  # Set to your own GLFW libs path or leave it as it is
+set (GLFW_INCLUDE_PATH ".\external\include")  # Set to your own GLFW include path or leave it as it is
+set (ASSIMP_LIB_PATH ".\external\lib")  # Set to your own Assimp path or leave it as it is
+set (ASSIMP_INCLUDE_PATH ".\external\include")  # Set to your own Assimp path or leave it as it is
+set (VMA_LIB_PATH ".\external\lib")  # Set to your own VMA path or leave it as it is
+set (VMA_INCLUDE_PATH ".\external\include")  # Set to your own VMA path or leave it as it is
+#set (VULKAN_LIB_PATH "C:/VulkanSDK/1.2.154.1/Lib")  # Optional. CMake will try to find Vulkan by itself.
+#set (VULKAN_INCLUDE_PATH "C:/VulkanSDK/1.2.154.1/Include")  # Optional. CMake will try to find Vulkan by itself.
+#set(MINGW_PATH "C:/msys64/mingw64")  # Optional. Set to your own MinGW path if using MinGW
